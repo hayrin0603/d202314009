f4ad31d5ccc9ceebd2fbbb3f41c9923a042aea62
hayrin0603 <hinoerak9@gmail.com>
2025-09-17 15:40:44 +0900

    five

--- src/d202314009/Main.java
+++ src/d202314009/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("전혜린");
+        System.out.println("안녕하세요");
+        System.out.println("hello world");
+        Hello.run();
     }
 
 }
